c3ecffaa6fc5e7f6b451a38fe7bb5e9b9351abb4
Daniel Gutierrez-Desarrollador <daniel.cet993@gmail.com>
2026-09-20 07:28:44 -0300

    iniciando proyecto

---
+++ src/main/java/com/danielgutierrez/api_finanza_personales/ApiFinanzaPersonalesApplication.java
@@ -0,0 +1,13 @@
+package com.danielgutierrez.api_finanza_personales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanzaPersonalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinanzaPersonalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielgutierrez/api_finanza_personales/ApiFinanzaPersonalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielgutierrez.api_finanza_personales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinanzaPersonalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
